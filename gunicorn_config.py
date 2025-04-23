de1b8fe1ed08f81fb2250623c8fa6b073e5011d7
Asish <z5430777@ad.unsw.edu.au>
2025-04-24 08:31:05 +1000

    deployed

---
+++ gunicorn_config.py
@@ -0,0 +1,19 @@
+import multiprocessing
+
+# Server socket
+bind = "0.0.0.0:5000"
+
+# Worker processes
+workers = multiprocessing.cpu_count() * 2 + 1
+worker_class = "sync"
+worker_connections = 1000
+timeout = 30
+keepalive = 2
+
+# Logging
+accesslog = "-"
+errorlog = "-"
+loglevel = "info"
+
+# Process naming
+proc_name = "financial_score_server" 
